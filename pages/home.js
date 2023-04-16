12c3df5221c0cd42da7ecea669a79a1e2c75bb86
kartikgx13 <108775715+kartikgx13@users.noreply.github.com>
2023-04-16 11:50:29 +0530

    kartik gavande 20th commit date: 16/04/2023

--- pages/home.js
+++ pages/home.js
@@ -6,24 +6,15 @@ import LoginForm from '../components/LoginForm';
 import TransactionWindow from '../components/TransactionWindow';
 
 
-function home(props) {
-  {/*const [transaction,updateTransaction]=useState([]);//since initially the list will be empty hence we have passed an empty array
-
-  //method to add the transactions to the transaction array
-  const addTransaction=(payload)=>{
-    //defining an array to store the transaction
-    const transactionArray= [...transaction]
-    transactionArray.push(payload);
-  updateTransaction(transactionArray);*/}
+function home() {
   return (
     <>
     <h1>Hello</h1>
-    {/*<Overview addTransaction={addTransaction}/>
-    <Transaction transaction={transaction}/>*/}
     <TransactionWindow/>
     </>
   )
   }
+
   
 
 export default home
