a03edb22f9cd7c9b334b80b17284be8c5d346509
kartikgx13 <108775715+kartikgx13@users.noreply.github.com>
2023-04-16 13:24:11 +0530

    kartik gavande 22nd commit date:16/04/2023

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,6 @@
 import './styles.css';
-import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    window.addEventListener('beforeunload', handleWindowBeforeUnload);
-
-    return () => {
-      window.removeEventListener('beforeunload', handleWindowBeforeUnload);
-    };
-  }, []);
-
-  const handleWindowBeforeUnload = () => {
-    localStorage.clear();
-  };
-
   return (
     <div>
       <Component {...pageProps} />
